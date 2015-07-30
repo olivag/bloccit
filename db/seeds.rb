d3c2507e2e32271bf8882b359de76cdca7f3c70f
Gerald Oliva <ryumaster001@yahoo.com>
2015-07-30 14:14:55 -0400

    Assignment 32 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,25 @@
  require 'faker'
  
- # Create Posts
- 50.times do
-   Post.create!(
-     title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
-   )
- end
- posts = Post.all
+Post.find_or_create_by!(title: "Testing Post", body: "This post will be added only once")
+ # # Create Posts
+ # 50.times do
+ #   Post.create!(
+ #     title:  Faker::Lorem.sentence,
+ #     body:   Faker::Lorem.paragraph
+ #   )
+ # end
+ # posts = Post.all
  
- # Create Comments
- 100.times do
-   Comment.create!(
-     post: posts.sample,
-     body: Faker::Lorem.paragraph
-   )
- end
+Comment.find_or_create_by!(body: "This comment will be added only once")
+#Comment.find_or_create_by!(post: Post.first, body: "This comment will be added only once")
+
+ # # Create Comments
+ # 100.times do
+ #   Comment.create!(
+ #     post: posts.sample,
+ #     body: Faker::Lorem.paragraph
+ #   )
+ # end
  
  puts "Seed finished"
  puts "#{Post.count} posts created"
